be02118c09cd33af9e91b0af3326bd29b81f1a86
Igor Zinovyev <zinigor@gmail.com>
2020-05-26 15:37:44 +0300

    Fixed the test to succeed.

--- tests/IniFile.test.ts
+++ tests/IniFile.test.ts
@@ -2,9 +2,11 @@ import IniFile from '../src/IniFile';
 import { expect } from 'chai';
 
 describe('The IniFile class', function() {
-    it('should parse JSON', function() {
-        let inputData: String = "{\"some_json\":\"data\"}";
-        let result = new IniFile(inputData);
-        expect(result).equal(7);
+    it('should use input data', function() {
+        const inputData: object = { some_json: "data" };
+        const result = new IniFile(inputData);
+
+        const parsedData = result.data;
+        expect(parsedData["some_json"]).equal("data");
     });
 });
